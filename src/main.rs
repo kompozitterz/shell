20ea68884945bf691a5b65f2861066408427cd61
kompozitterz <kbatomene@gmail.com>
2024-12-06 14:09:24 +0100

    Initialisation du terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // Afficher le prompt 
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        // Lire l'entrée utilisateur 
+        let mut input = String::new();
+        if let Err (_) = io::stdin().read_line(&mut input) {
+            println!("Erreur lors de la lecture de l'entrée. Veuillez ressayez s'il vous plait!");
+            continue;
+        }
+
+        // Supprimer les espaces superflus 
+        let input = input.trim();
+
+        // Vérifier la commmande 'exit' pour quitter le shell 
+        if input == "exit" {
+            println!("Bye!");
+            break;
+        }
+
+        // Afficher la coommande (pour test)
+        println!("Vous avez tapé: {}", input);
+    }
 }
